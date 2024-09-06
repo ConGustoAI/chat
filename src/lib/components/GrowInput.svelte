023de7cfd76d377befefb629701e595c01a94f05
juddydev <artoh1124@gmail.com>
2024-09-06 18:03:41 +0900

    fix paste into chat input

--- src/lib/components/GrowInput.svelte
+++ src/lib/components/GrowInput.svelte
@@ -3,38 +3,47 @@
 
 	import dbg from 'debug';
 	const debug = dbg('app:ui:components:GrowInput');
-
+		
 	export let value = '';
-	let className = ''; // Keep this as a local variable
+	let className = ''; 
 	export { className as class }; // Export it as 'class'
 	export let disabled = false;
 	export let spellcheck = false;
 
 	let textBox: HTMLDivElement | null = null;
 
-	// function handlePaste(event: ClipboardEvent) {
-	// 	event.preventDefault();
-	// 	const text = event.clipboardData?.getData('text/plain');
-	// 	if (!textBox || !text) return;
-	// 	textBox.innerText = text;
-	// 	value = text;
-	// }
+	// Handle the paste event manually
+	function handlePaste(event: ClipboardEvent) {
+		event.preventDefault();
+		const text = event.clipboardData?.getData('text/plain');
+		if (!textBox || !text) return;
+		document.execCommand('insertText', false, text);
+		value = textBox.innerText;
+	}
+
+	// Update value on input changes
+	function handleInput() {
+		if (textBox) {
+			value = textBox.innerText;
+		}
+	}
+
+	$: if (textBox && textBox.innerText !== value) {
+		textBox.innerText = value;
+	}
 </script>
 
 {#if disabled}
 	<div class={cn('textarea h-fit min-h-10 w-full resize-none overflow-auto py-2 text-base', className)}>{value}</div>
 {:else}
-	<!-- svelte-ignore a11y-no-noninteractive-element-to-interactive-role -->
 	<div
 		tabindex={0}
 		role="textbox"
 		contenteditable
 		{spellcheck}
 		bind:this={textBox}
-		bind:innerText={value}
+		on:input={handleInput}
+		on:paste={handlePaste}
 		on:keydown
-		on:change
-		on:input
-		class={cn('textarea min-h-10 overflow-y-auto py-2 text-base whitespace-pre-wrap', className)}>
-	</div>
+		class={cn('textarea min-h-10 overflow-y-auto py-2 text-base whitespace-pre-wrap', className)} />
 {/if}
