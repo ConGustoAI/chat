c0c897fa6502f7dec3d04812dc00003421a47c34
Alexey <alexey.zaytsev@gmail.com>
2024-09-08 18:27:27 -0500

    Switch to adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 // import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
