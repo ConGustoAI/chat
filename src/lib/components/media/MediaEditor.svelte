e8298ec645ba39b5efc56e0a933fe282050b0108
Alexey <alexey.zaytsev@gmail.com>
2024-10-21 10:19:51 -0500

    Make the text edit window scrollable

--- src/lib/components/media/MediaEditor.svelte
+++ src/lib/components/media/MediaEditor.svelte
@@ -71,7 +71,7 @@
 					alt={A.mediaEditing.title}
 					class="pixilated bg-checkered shrink grow self-stretch overflow-hidden object-contain" />
 			{:else if A.mediaEditing?.type === 'text'}
-				<div class="flex h-full max-h-full grow flex-col p-2">
+				<div class="flex h-full max-h-full grow flex-col p-2 overflow-scroll">
 					<GrowInput
 						bind:value={A.mediaEditing.original!.text}
 						oninput={() => (textNeedsSave = true)}
